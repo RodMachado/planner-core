cb907601590ad699b7985a70067c0d1e8da286bb
Rodrigo Machado <rodmac98@gmail.com>
2016-02-01 20:07:51 -0500

    Switching db cleaner strategy

--- spec/features/autentication/user_login_spec.rb
+++ spec/features/autentication/user_login_spec.rb
@@ -1,8 +1,31 @@
 require 'rails_helper'
 
 feature 'User Login' do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:role) { FactoryGirl.create(:admin_role) }
+  let!(:role_assignment) do
+    FactoryGirl.create(
+      :role_assignment,
+      user: user,
+      role: role
+    )
+  end
+
   scenario 'basic access' do
     visit '/'
-    expect(page).to have_content('MyConvention')
+    fill_in('user_login', with: 'login')
+    fill_in('user_password', with: 'password')
+    click_button 'Log In'
+
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario 'wrong password' do
+    visit '/'
+    fill_in('user_login', with: 'login')
+    fill_in('user_password', with: 'badpassword')
+    click_button 'Log In'
+
+    expect(page).to have_content('Invalid email or password.')
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :truncation, { except: %w(translations enumrecord) }
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, js: true) do
